ed4937b42cc63055be88e75a6fe8ab212a509379
Joe Liotta <jliotta03@gmail.com>
2025-02-27 12:23:22 -0500

    0.0.6 json

--- src/xlsxparse_joeypas/parse.py
+++ src/xlsxparse_joeypas/parse.py
@@ -1,10 +1,31 @@
 from openpyxl import load_workbook
+from openpyxl.worksheet.cell_range import CellRange
 from pathlib import PureWindowsPath
 from urllib.parse import unquote
 import re
 
+
+defined_ranges = []
+
+def build_defined_ranges(defined_names):
+    defined_names = [x for x in defined_names.items() if not x[0].startswith("_xlchart")]
+    for name in defined_names:
+        defn = name[0]
+        for title, coord in name[1].destinations:
+            defined_ranges.append({
+                'name': defn,
+                'sheet': title,
+                'range': CellRange(coord),
+            })
+
+def is_defined(cell):
+    sheet_name = cell.parent.title
+    coord = cell.coordinate
+    ranges = [x['name'] for x in defined_ranges if x['sheet'] == sheet_name and not x['range'].isdisjoint(CellRange(coord))]
+    return ranges
+
 def extract(formula, curr_sheet, links):
-    ref_pattern = r"(?:(?:\[([^\]]+)\])?([^'=,]+)'?\!)?([A-Z|$]+\d+(?::[A-Z|$]+\d+)?)"
+    ref_pattern = r"(?:(?: *'?\[([^\]]+)\])?([^'=,]+)'?\!)?([A-Z|$]+\d+(?::[A-Z|$]+\d+)?)"
     matches = re.findall(ref_pattern, formula)
     refs = []
 
@@ -22,7 +43,11 @@ def extract(formula, curr_sheet, links):
 
     return refs
 
-def getNames(sheet, cell):
+def getNames(cell):
+    defd = is_defined(cell)
+    if (len(defd) > 0):
+        return tuple(defd)
+    sheet = cell.parent
     row_label = None
     col_label = None
     i = cell.row
@@ -46,7 +71,7 @@ def parse_excel_formulas(sheet, links):
     for row in sheet.iter_rows():
         for cell in row:
             if (isinstance(cell.value, str) and cell.value.startswith('=')):
-                names = getNames(sheet, cell)
+                names = getNames(cell)
                 formulas[cell.coordinate] = {
                     "names": names,
                     "formula": cell.value,
@@ -57,6 +82,7 @@ def parse_excel_formulas(sheet, links):
 
 def parse_all_sheets(file_path):
     wb = load_workbook(file_path, data_only=False)
+    build_defined_ranges(wb.defined_names)
     all_refs = {}
 
     for sheet in wb.sheetnames:
@@ -69,5 +95,6 @@ def parse_all_sheets(file_path):
 def parse_single_sheet(file_path, sheet_name):
     wb = load_workbook(file_path, data_only=False)
     sheet = wb[sheet_name]
+    build_defined_ranges(wb.defined_names)
 
     return parse_excel_formulas(sheet, wb._external_links)
