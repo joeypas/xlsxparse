d372da5613c2ce3e809db50ef0f06f5fbaa3db1a
Joe Liotta <jliotta03@gmail.com>
2025-03-06 12:15:44 -0500

    README and refine

--- src/xlsxparse_joeypas/cli.py
+++ src/xlsxparse_joeypas/cli.py
@@ -1,12 +1,12 @@
 import typer
 import json
 from xlsxparse_joeypas.parse import parse_all_sheets, parse_single_sheet
-from xlsxparse_joeypas.search import search_cell, search_ref_file, search_ref_sheet, search_ref_file_sheet
+from xlsxparse_joeypas.search import search_ref_file, search_ref_sheet, search_ref_file_sheet, search_metric, search_sheet_metric
 from typing_extensions import Annotated
 from typing import Optional
 from enum import Enum
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 def version_callback(value: bool):
     if value:
@@ -18,75 +18,57 @@ app = typer.Typer(no_args_is_help=True)
 @app.command()
 def create(
     file: Annotated[str, typer.Argument(help="Path to the .xlsx WorkBook")],
-    sheet_name: Annotated[str, typer.Argument(help="Name of the sheet to parse (if none provided, parse all sheets)")] = None,
-    to_json: Annotated[bool, typer.Option("--json", help="Output file format as json.")] = False,
+    sheet_name: Annotated[str, typer.Option("--sheet", help="Name of the sheet to parse (if none provided, parse all sheets)")] = None,
     verbose: Annotated[bool, typer.Option("--verbose", help="Write output to console")] = False,
-    output_file: Annotated[str, typer.Option(help="Name of file to output")] = "output",
+    output_file: Annotated[str, typer.Option("--output", help="Path of output file")] = "output.json",
     version: Annotated[Optional[bool], typer.Option("--version", callback=version_callback)] = None,
 ):
-    if to_json:
-        with open(output_file + ".json", "w+") as txt_file:
-            out = []
-            if sheet_name:
-                refs = parse_single_sheet(file, sheet_name)
-                for cell, data in refs.items():
+    with open(output_file, "w+") as json_file:
+        out = []
+        if sheet_name:
+            refs = parse_single_sheet(file, sheet_name)
+            for cell, data in refs.items():
+                out.append({
+                    'Sheet': sheet_name, 'Metric': data['names'],
+                    'Cell': cell, 'Formula': data['formula'], 'References': data['references']
+                })
+                if verbose:
+                    print(f"Cell: {cell}, Formula: {data['formula']}, Refrences: {data['references']}")
+        else:
+            refs = parse_all_sheets(file)
+            for sheet, item in refs.items():
+                for cell, data in item['items'].items():
                     out.append({
-                        'Sheet': sheet_name, 'Metric': data['names'],
+                        'Sheet': sheet, 'Metric': data['names'],
                         'Cell': cell, 'Formula': data['formula'], 'References': data['references']
                     })
                     if verbose:
-                        print(f"Cell: {cell}, Formula: {data['formula']}, Refrences: {data['references']}")
-            else:
-                refs = parse_all_sheets(file)
-                for sheet, item in refs.items():
-                    for cell, data in item['items'].items():
-                        out.append({
-                            'Sheet': sheet, 'Metric': data['names'],
-                            'Cell': cell, 'Formula': data['formula'], 'References': data['references']
-                        })
-                        if verbose:
-                            print(f"Sheet: {sheet}, Metrics: {data['names']}, Cell: {cell}, Formula: {data['formula']}, References: {data['references']}")
+                        print(f"Sheet: {sheet}, Metrics: {data['names']}, Cell: {cell}, Formula: {data['formula']}, References: {data['references']}")
 
-            txt_file.write(json.dumps(out, indent=2))
-    else:
-        with open(output_file + ".txt", "w+") as txt_file:
-            out = []
-            if sheet_name:
-                refs = parse_single_sheet(file, sheet_name)
-                for cell, data in refs.items():
-                    out.append(f"Sheet: {sheet}, Metrics: {data['names']}, Cell: {cell}, Formula: {data['formula']}, References: {data['references']}\n")
-                    if verbose:
-                        print(f"Cell: {cell}, Formula: {data['formula']}, Refrences: {data['references']}")
-            else:
-                refs = parse_all_sheets(file)
-                for sheet, item in refs.items():
-                    for cell, data in item['items'].items():
-                        out.append(f"Sheet: {sheet}, Metrics: {data['names']}, Cell: {cell}, Formula: {data['formula']}, References: {data['references']}\n")
-                        if verbose:
-                            print(f"Sheet: {sheet}, Metrics: {data['names']}, Cell: {cell}, Formula: {data['formula']}, References: {data['references']}")
+        json_file.write(json.dumps(out, indent=2))
 
-            txt_file.writelines(out)
 
 class SearchType(str, Enum):
-    cell = "cell"
-    sheet = "sheet"
-    file = "file"
-    file_sheet = "file-sheet"
+    metric = "metric"
+    sheet_metric = "sheet-metric"
+    ref_file = "ref-file"
+    ref_file_sheet = "ref-file-sheet"
 
 @app.command()
 def search(
-    file: Annotated[str, typer.Argument(help="Path to the file.")],
     string: Annotated[str, typer.Argument(help="String to search for")],
-    stype: Annotated[SearchType, typer.Option(case_sensitive=False)] = SearchType.cell,
+    file: Annotated[str, typer.Argument(help="Path to the file.")] = "output.json",
+    stype: Annotated[SearchType, typer.Option(case_sensitive=False)] = SearchType.metric,
 ):
     with open(file, "r") as txt:
         contents = json.loads(str(txt.read()))
-        if (stype.value == SearchType.cell):
-            print(json.dumps(search_cell(contents, string), indent=2))
-        elif (stype.value == SearchType.file):
+        if (stype.value == SearchType.ref_file):
             print(json.dumps(search_ref_file(contents, string), indent=2))
-        elif (stype.value == SearchType.sheet):
-            print(json.dumps(search_ref_sheet(contents, string), indent=2))
+        elif (stype.value == SearchType.metric):
+            print(json.dumps(search_metric(contents, string), indent=2))
+        elif (stype.value == SearchType.sheet_metric):
+            sstring = string.split(", ")
+            print(json.dumps(search_sheet_metric(contents, sstring[0], sstring[1]), indent=2))
         else:
             sstring = string.split(", ")
             print(json.dumps(search_ref_file_sheet(contents, sstring[0], sstring[1]), indent=2))
--- src/xlsxparse_joeypas/search.py
+++ src/xlsxparse_joeypas/search.py
@@ -1,6 +1,3 @@
-def search_cell(contents, cell: str):
-    return [x for x in j if x["Cell"] == cell]
-
 def search_ref_sheet(contents, sheet_name: str):
     return [obj for obj in contents if any(ref['sheet'] == sheet_name for ref in obj['References'])]
 
@@ -10,3 +7,8 @@ def search_ref_file(contents, file_name: str):
 def search_ref_file_sheet(contents, file_name: str, sheet_name: str):
     return [obj for obj in contents if any("file" in ref and ref["file"] == file_name and ref["sheet"] == sheet_name for ref in obj['References'])]
 
+def search_metric(contents, metric: str):
+    return [obj for obj in contents if metric in obj["Metric"]]
+
+def search_sheet_metric(contents, sheet_name: str, metric: str):
+    return [obj for obj in contents if metric in obj["Metric"] and obj["Sheet"] == sheet_name]
